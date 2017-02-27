646e4c4d15deaee48a75cdf24fa6628b88e91bb7
Jim Rong-Zhu <zhur_jim@163.com>
2017-02-27 16:44:22 +0800

    del

---
+++ src/NIO/BufferFillDrain2.java
@@ -0,0 +1,43 @@
+package NIO;
+
+import java.nio.CharBuffer;
+
+public class BufferFillDrain2 {
+	public static void main(String[] argv) throws Exception {
+		CharBuffer buffer = CharBuffer.allocate(100);
+		while (fillBuffer(buffer)) {
+			buffer.flip();
+			drainBuffer(buffer);
+			buffer.clear();
+		}
+	}
+
+	private static void drainBuffer(CharBuffer buffer) {
+		while (buffer.hasRemaining()) {
+			System.out.print(buffer.get());
+		}
+		System.out.println("");
+	}
+
+	private static boolean fillBuffer(CharBuffer buffer) {
+		if (index >= strings.length) {
+			return (false);
+		}
+		String string = strings[index++];
+		for (int i = 0; i < string.length(); i++) {
+			buffer.put(string.charAt(i));
+		}
+		// 缓冲区并不是多线程安全的。如果您想以多线程同时存取特定
+		// 的缓冲区，您需要在存取缓冲区之前进行同步（例如对缓冲区
+		// 对象进行跟踪）。
+		// 29
+		return (true);
+	}
+
+	private static int index = 0;
+	private static String[] strings = { "A random string value", "The product of an infinite number of monkeys",
+			"Hey hey we're the Monkees", "Opening act for the Monkees: Jimi Hendrix", "'Scuse me while I kiss this fly", // Sorry
+																															// Jimi
+																															// ;-)
+			"Help Me! Help Me!", };
+}
